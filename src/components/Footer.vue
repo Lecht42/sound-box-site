12fe6f95ee50221dc3f1e290d074c6c4b155c050
Lecht <lecht42@gmail.com>
2026-02-01 15:30:28 +0200

    Change span to footer element in Footer.vue for semantic HTML

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <span class="w-full bg-primary py-4 flex flex-row justify-center font-tektur text-lg md:text-xl">
+    <footer class="w-full bg-primary py-4 flex flex-row justify-center font-tektur text-lg md:text-xl">
         © {{ year }} Sound Box. Все права защищены.
-    </span>
+    </footer>
 </template>
