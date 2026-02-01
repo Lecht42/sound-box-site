a4b26852607e58cf688ee95c3610746e135b7e66
Lecht <lecht42@gmail.com>
2026-02-01 15:49:20 +0200

    Refactor Menu component to use dynamic routing and improve current route styling

--- src/components/content/Menu.vue
+++ src/components/content/Menu.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
+import { useRoute } from "vue-router";
+import {router} from '../../router';
 
+const routes = router.getRoutes();
+const route = useRoute();
+const isCurrent = (path: string) => route.path === path;
+const isCurrentStyle = "text-link underline";
 </script>
 
 <template>
     <span class="w-full bg-primary py-2 flex flex-row justify-around font-tektur text-lg md:text-xl">
-        <router-link to="/">
-            <h1>Главная</h1>
+        <router-link :to="routes[0].path">
+            <h1 :class="isCurrent(routes[0].path) && isCurrentStyle">Главная</h1>
         </router-link>
-        <router-link to="/services">
-            <h1>Товары и услуги</h1>
+        <router-link :to="routes[1].path">
+            <h1 :class="isCurrent(routes[1].path) && isCurrentStyle">Товары и услуги</h1>
         </router-link>
-        <router-link to="/blog">
-            <h1>Новости</h1>
+        <router-link :to="routes[2].path">
+            <h1 :class="isCurrent(routes[2].path) && isCurrentStyle">Новости</h1>
         </router-link>
     </span>
 </template>
