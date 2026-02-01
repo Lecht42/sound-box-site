a0b886335b9cd2b200580399f1891a846aa086f6
Lecht <lecht42@gmail.com>
2026-02-01 15:31:09 +0200

    Refactor Content and Menu components for improved structure; update HomeView headings and adjust styles in CSS

--- src/components/content/Content.vue
+++ src/components/content/Content.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
+import Contacts from './Contacts.vue';
 import Menu from './Menu.vue';
 </script>
 
 <template>
     <div class="w-full h-full min-h-screen bg-secondary rounded-lg overflow-clip flex flex-col items-center justify-start">
         <Menu />
-        <div class="w-full p-4 md:p-12 text-text">
+        <div class="w-full p-4 md:p-12">
             <router-view />
         </div>
     </div>
+    <Contacts />
 </template>
--- src/components/content/Menu.vue
+++ src/components/content/Menu.vue
@@ -3,9 +3,15 @@
 </script>
 
 <template>
-    <span  class="w-full bg-primary py-2 flex flex-row justify-around font-tektur text-lg md:text-xl">
-        <router-link to="/">Главная</router-link>
-        <router-link to="/services">Товары и услуги</router-link>
-        <router-link to="/blog">Новости</router-link>
+    <span class="w-full bg-primary py-2 flex flex-row justify-around font-tektur text-lg md:text-xl">
+        <router-link to="/">
+            <h1>Главная</h1>
+        </router-link>
+        <router-link to="/services">
+            <h1>Товары и услуги</h1>
+        </router-link>
+        <router-link to="/blog">
+            <h1>Новости</h1>
+        </router-link>
     </span>
 </template>
