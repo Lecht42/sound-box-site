73337bd9add11e743731c8afafaed1d2c47544c7
Lecht <lecht42@gmail.com>
2026-02-01 14:56:21 +0200

    Add Footer component to App.vue and create Footer.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
   import Logo from './components/Logo.vue';
   import Content from './components/content/Content.vue';
+  import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Logo />
   <Content />
+  <Footer />
 </template>
 
 <style scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    const year = new Date().getFullYear();
+</script>
+
+<template>
+    <span class="w-full bg-primary py-4 flex flex-row justify-center font-tektur text-lg md:text-xl">
+        © {{ year }} Sound Box. Все права защищены.
+    </span>
+</template>
